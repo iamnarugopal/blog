55f77fa6efd641c102d872f0a86d186d19191b56
Naru Gopal <veeruk0217@gmail.com>
2023-05-03 09:38:52 +0530

    commit

--- middleware/uploadImage.js
+++ middleware/uploadImage.js
@@ -1,32 +1,74 @@
+import { handleUpload } from "./cloudinary";
 const multer = require("multer");
 
 //Configuration for Multer
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images/blogs");
-  },
-  filename: (req, file, cb) => {
-    const ext = file.mimetype.split("/")[1];
-    cb(null, `blog-${Date.now()}.${ext}`);
-  },
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "images/blogs");
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = file.mimetype.split("/")[1];
+//     cb(null, `blog-${Date.now()}.${ext}`);
+//   },
+// });
 
-// Multer Filter
-const multerFilter = (req, file, cb) => {
-  if (
-    file.mimetype.split("/")[1] === "jpg" ||
-    file.mimetype.split("/")[1] === "png" ||
-    file.mimetype.split("/")[1] === "jpeg"
-  ) {
-    cb(null, true);
-  } else {
-    cb(new Error("Invalid image file"), false);
+// // Multer Filter
+// const multerFilter = (req, file, cb) => {
+//   if (
+//     file.mimetype.split("/")[1] === "jpg" ||
+//     file.mimetype.split("/")[1] === "png" ||
+//     file.mimetype.split("/")[1] === "jpeg"
+//   ) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("Invalid image file"), false);
+//   }
+// };
+
+// const uploadImage = multer({
+//   storage: multerStorage,
+//   fileFilter: multerFilter,
+// });
+
+// module.exports = uploadImage;
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+const myUploadMiddleware = upload.single("image");
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+const handler = async (req, res) => {
+  const { method } = req;
+  if (method === "OPTIONS") {
+    return res.status(200).send("ok");
+  }
+  try {
+    await runMiddleware(req, res, myUploadMiddleware);
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+    const cldRes = await handleUpload(dataURI);
+    res.json(cldRes);
+  } catch (error) {
+    console.log(error);
+    res.send({
+      message: error.message,
+    });
   }
 };
+export default handler;
 
-const uploadImage = multer({
-  storage: multerStorage,
-  fileFilter: multerFilter,
-});
-
-module.exports = uploadImage;
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
